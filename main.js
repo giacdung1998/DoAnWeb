859e0417ac5ee894b7fb098f07dc3857a8dab20c
giacdung1998 <45871891+giacdung1998@users.noreply.github.com>
2018-12-14 22:04:06 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+	/*$('.menuslidebar>li>a>.title').after('<span class="arrow dashicons dashicons-arrow-right-alt2"></span>');
+	//khi lick vào cấp 1
+	$('.menuslidebar>li').click(function(){
+		if($(this).hasClass('active')){
+			$(this).children('.submenu').slideUp();
+		}
+		else{
+			$(this).children('submenu').slideDown();
+		}
+	});*/
