b854563988fc97c9824b7cec66695151ee37193d
giacdung1998 <45871891+giacdung1998@users.noreply.github.com>
2018-12-21 22:42:11 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+</div>
+  <!-- /.container-fluid -->
+         </div>
+         <!-- /#page-wrapper -->
+      </div>
+      <!-- /#wrapper -->
+      <!-- jQuery -->
+      <script src="/doanweb/public/admin/js/jquery.js"></script>
+      <!-- Bootstrap Core JavaScript -->
+      <script src="/doanweb/public/admin/js/bootstrap.min.js"></script>
+   </body>
+</html>
